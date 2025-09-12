a6b0210bc83cffd5cfe4b41286b1c147787af92e
Domenico Lanza <42304229+domilanza2002@users.noreply.github.com>
2025-09-12 12:01:14 -0500

    Add license headers to Python files

--- runner.py
+++ runner.py
@@ -1,3 +1,28 @@
+"""
+@file runner.py
+
+@brief Convenience script to run main.py on multiple XLSX cases.
+
+Copyright (C) 2023-2025 The Board of Trustees of the University of Illinois.
+All rights reserved.
+
+This file is part of PlasFlowSolver: a data reduction model for ICP wind tunnels.
+
+PlasFlowSolver is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
+
+PlasFlowSolver is distributed in the hope that it will be useful, 
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with PlasFlowSolver.  If not, see 
+<http://www.gnu.org/licenses/>.
+"""
+
 # This is not a project file, but a script that can be used to run the main.py script on multiple xlsx files
 
 import subprocess  # For running python scripts from python
